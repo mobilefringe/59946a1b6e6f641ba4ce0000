928c849e9565d705956a5711db227c6199c0ca6b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-09-29 16:08:21 -0400

    Devonshire Mall 1.1  /application.js

--- application.js
+++ application.js
@@ -119,7 +119,7 @@ function renderEventDetails(container, template, collection, mall_name){
             }
         } else {
             val.store_name = mall_name;
-            val.store_image = "//codecloud.cdn.speedyrails.net/sites/59946a1b6e6f641ba4ce0000/image/png/1502995441000/default.png";
+            val.store_image = "//codecloud.cdn.speedyrails.net/sites/59c082786e6f6462ee1d0000/image/jpeg/1506715414000/devonshire_default.jpg";
             val.store_show = "display:none";
             val.phone_show = "display:none";
             val.show = "display:none";
